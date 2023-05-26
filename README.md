ba91326ea91fc35299fa8b3f009e04e95010750d
lnreinhard <88722384+lnreinhard@users.noreply.github.com>
2023-05-26 15:08:28 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Introduction
+This project provides the source code for a Google Cloud Function that interacts with the Strava API, retrieves activity data, and stores it in a Google Cloud Storage bucket. The function is designed to be executed by a scheduler every 30 minutes, ensuring that the latest activity data is regularly fetched and stored.
+
+# Code Explanation
+
+The primary functionalities of the code include:
+
+1. Fetching data from the Strava API: The code integrates with the Strava API to retrieve activity data. This includes information such as distance, duration, elevation, and more for each activity.
+
+2. Converting activities to JSON: The fetched activity data is then transformed into JSON format, making it easy to work with and analyze.
+
+3. Writing to Google Cloud Storage: The resulting JSON file is written to a designated Google Cloud Storage bucket. This allows for efficient storage and enables further processing or analysis of the activity data.
+
+The Google Cloud Function, powered by this code, serves as an automated solution for keeping the activity data up-to-date in a centralized storage location. By executing the function on a scheduled basis, it ensures that the most recent activities are consistently captured and made available for downstream processes or analysis.
+
+# Usage
+
+To utilize this Google Cloud Function and automate the retrieval and storage of Strava activity data, follow these steps:
+
+**1. Prerequisites:** Before getting started, ensure you have the following prerequisites in place:
+
+* A Google Cloud Platform (GCP) project with billing enabled.
+* The Google Cloud SDK (gcloud) installed and authenticated with your GCP project.
+* A Strava developer account with an active application and the necessary API credentials.
+
+**2. Setup:**
+
+* Clone or download the source code from this repository.
+* Configure your Google Cloud project and set up a Google Cloud Storage bucket to store the activity data.
+
+**3. Configuration:**
+* Update the necessary environment variables or configuration values in the code:
+  * Provide your Strava API credentials (client ID, client secret, etc.) to authorize access to the Strava API.
+  * Specify the details of your Google Cloud Storage bucket (bucket name, authentication credentials, etc.).
+
+**4. Deployment:**
+* Deploy the Google Cloud Function using the provided deployment command or script.
+* Configure the scheduler to trigger the function execution every 30 minutes. You can utilize GCP's Cloud Scheduler service for this purpose.
+
+**5. Testing and Monitoring:**
+
+* Test the function by triggering it manually or waiting for the scheduler to execute it.
+* Monitor the function's logs and Cloud Storage bucket to ensure the data is being fetched and stored correctly.
---
+++ main.py
@@ -0,0 +1,100 @@
+# IMPORT MODULES
+import os
+from tqdm import tqdm
+import json
+import pandas as pd
+import requests
+import urllib3
+from google.cloud import storage
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+def hello_pubsub(event, context):
+
+    #SET ENVIRONMENT VARIABLES
+    client_id = os.environ.get("client_id")
+    client_secret = os.environ.get("client_secret")
+    refresh_token = os.environ.get("refresh_token")
+
+    # DEFINE FUNCTION TO GET NEW ACCESS TOKEN
+    def get_access_token(client_id, client_secret, refresh_token):
+    
+        oauth_url = 'https://www.strava.com/oauth/token'
+    
+        payload = {
+            'client_id': client_id, 
+            'client_secret': client_secret, 
+            'refresh_token': refresh_token, 
+            'grant_type': 'refresh_token', 
+            'f': 'json', 
+        }
+    
+        r = requests.post(oauth_url, data=payload, verify=False, timeout=30)
+        
+        access_token = r.json()['access_token']
+        return access_token
+    
+
+    # GET NEW ACCESS TOKEN
+    print("Getting new access token...")
+    access_token = get_access_token(client_id, client_secret, refresh_token)
+    print("Access token obtained:", access_token)
+
+
+    # DEFINE FUNCTION TO GET STRAVA DATA
+    def get_data(access_token, per_page=200, page=1):
+    
+        activities_url = 'https://www.strava.com/api/v3/athlete/activities'
+        headers = {'Authorization': 'Bearer ' + access_token}
+        params = {'per_page': per_page, 'page': page}
+        
+        data = requests.get(
+            activities_url, 
+            headers=headers, 
+            params=params
+        ).json()
+        
+        return data
+
+    # GET STRAVA DATA
+    max_number_of_pages = 10
+    data = list()
+    print("Fetching Strava data...")
+    for page_number in tqdm(range(1, max_number_of_pages + 1)):
+        page_data = get_data(access_token, page=page_number)
+        if page_data == []:
+            break
+        data.append(page_data)
+    print("Strava data fetched.")
+    
+
+    # DATA DICTIONARIES
+    data_dictionaries = []
+    for page in data:
+        data_dictionaries.extend(page)
+
+
+    # PRINT NUMBER OF ACTIVITIES
+    print('Number of activities downloaded: {}'.format(len(data_dictionaries)))
+
+    data_json = json.dumps(data_dictionaries)
+
+    storage_client = storage.Client()
+    bucket_name = 'lnr-strava-bucket'
+    bucket = storage_client.bucket(bucket_name)
+
+
+    print("Uploading data to Google Cloud Storage...")
+
+    def upload_to_bucket(blob_name, file, bucket_name, content_type):
+        try:
+            bucket = storage_client.get_bucket(bucket_name)
+            blob = bucket.blob(blob_name)
+            blob.upload_from_string(file, content_type)
+            return True
+        except Exception as e:
+            print(e)
+            return False
+
+    upload_to_bucket("strava_raw", data_json, "lnr-strava-bucket", "application/json")
+    print("Data upload completed.")
